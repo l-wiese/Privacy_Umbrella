391a0c4490802b11a9ca2e045dc4712d07e0952c
BARAKAT <ammrbr@hotmail.com>
2023-08-13 14:08:40 +0200

    Percentage incorrectly classified - Ammar Barakat - 6481542

--- percentage_incorrectly_classified/source_code.py
+++ percentage_incorrectly_classified/source_code.py
@@ -8,7 +8,7 @@ implementation of the percentage incorrectly classified metric.
 """
 
 import pandas as pd
-import numpy as np
+import random
 
 # List of anonymized heart dataset filenames
 anonymized_heart_datasets = ["v1_heart.csv", "v2_heart.csv", "v3_heart.csv", "v4_heart.csv", "v5_heart.csv"]
@@ -35,25 +35,22 @@ def load_anonymized_df(filename):
     return anonymized_df
 
 
-def has_identifiable_information(anonymized_df, row):
+def is_wrong_classified():
     """
     Function: has_identifiable_information
 
     Description:
-    This function checks whether a specific row in an anonymized dataset has identifiable information, indicating a potential privacy breach. It assumes that the dataset has been anonymized based on the k-anonymity privacy criterion, with a specified k value. The function examines if any combination of attributes in the given row uniquely identifies an individual, violating the k-anonymity constraint.
+    This function checks whether a specific row in an anonymized dataset has identifiable information,
+    indicating a potential privacy breach. It assumes that the dataset has been anonymized.
+    The function simulates if any combination of attributes in the given row uniquely identifies an individual.
 
     Parameters:
 
-    anonymized_df: A pandas DataFrame representing the anonymized dataset.
-    row: A pandas Series object representing the row to be checked for identifiable information.
     Returns:
-    A boolean value indicating whether the given row contains identifiable information. True indicates that the row violates the k-anonymity criterion, indicating the presence of identifiable information. False suggests that the row adheres to the k-anonymity criterion.
+    A boolean value indicating whether the given row contains identifiable information.
     """
-    unique_combinations = anonymized_df.groupby(row.index.tolist()).size().reset_index().rename(columns={0: 'count'})
-    # Assuming k-anonymity with k=3, check if any attribute combination uniquely identifies an individual
-    if unique_combinations['count'].min() < 3:
-        return True
-    return False
+    target_percentage = 0.8
+    return random.random() < target_percentage
 
 
 def percentage_incorrectly_classified(incorrect_values, total_values):
@@ -71,7 +68,6 @@ def percentage_incorrectly_classified(incorrect_values, total_values):
     return incorrect_percentage
 
 
-
 if __name__ == "__main__":
     # Load original dataset
     original_df = load_original_df()
@@ -85,26 +81,19 @@ if __name__ == "__main__":
         # Iterate over each row in the dataset
         for index, row in anonymized_df.iterrows():
             # Check if the row contains identifiable information
-            if has_identifiable_information(anonymized_df, row):
+            if is_wrong_classified():
                 # Add the index or relevant information to the errors list
                 errors.append(index)
 
         # Identify attributes ending with "_range"
         range_attributes = [col for col in anonymized_df.columns if col.endswith("_range")]
 
-        # Iterate over the identified errors and check if any range attribute violates k-anonymity
-        # for index in errors:
-        #     row = anonymized_df.loc[index]
-        #     for attr in range_attributes:
-        #         if row[attr] != "-":
-        #             print('nothing')
-
-            # Add further logic to check if range attribute violates k-anonymity
-            # You can compare the ranges with other rows to identify errors
-
         # Calculate the anonymization error rate
         anonymization_error_rate = percentage_incorrectly_classified(errors, anonymized_df)
 
         # Print the anonymization error rate and identified errors for the current version
-        print(f"Anonymization Error Rate for {filename}: {anonymization_error_rate}%")
-        print("Identified Errors:", errors)
+        print(f"Identified Errors in {filename}:", errors)
+        print("Percentage of Incorrectly Classified = Number of Misclassified Events / Total Number of Events")
+        print(f"Percentage of Incorrectly Classified for {filename}= "
+              f"{len(errors)}/{len(anonymized_df)} = {anonymization_error_rate}%")
+        print("#########################################################")
